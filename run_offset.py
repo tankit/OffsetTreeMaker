908e58289f5dda31776c780fb77190d4759d997a
Minsuk Kim <minsuk@cern.ch>
2019-02-19 09:15:49 +0100

    update the codes for miniaod

--- run_offset.py
+++ run_offset.py
@@ -10,23 +10,29 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 process.options.allowUnscheduled = cms.untracked.bool(True)
 
 readFiles = cms.untracked.vstring()
 process.source = cms.Source ("PoolSource", fileNames = readFiles)
 readFiles.extend( [
-  '/store/data/Run2017B/ZeroBias/AOD/17Nov2017-v1/20000/00D69F39-6BD3-E711-8F55-44A842CF05E6.root'
+  #'/store/data/Run2017B/ZeroBias/AOD/17Nov2017-v1/20000/00D69F39-6BD3-E711-8F55-44A842CF05E6.root'
+  'file:/afs/cern.ch/user/m/minsuk/work/hip/OffsetTreeMaker/F68195F9-4343-E811-AA9F-002590D9D8C0.root'
+  #'/store/mc/RunIIFall17MiniAODv2/QCD_Pt-15to7000_TuneCUETHS1_Flat_13TeV_herwigpp/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/70000/FE935FB1-DD44-E811-B398-0CC47AA53D66.root'
 ] );
 
-isMC = cms.bool(False)
+#isMC = cms.bool(False)
+isMC = cms.bool(True)
 
 if isMC:
   OutputName = "_MC"
+  print "MC" 
+  process.load( "Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff" )
+  from Configuration.AlCa.GlobalTag import GlobalTag
+  process.GlobalTag = GlobalTag( process.GlobalTag, '94X_mc2017_realistic_v14' )
+  #process.GlobalTag.globaltag = cms.string("80X_mcRun2_asymptotic_2016_miniAODv2_v0")
 
-  #process.load( "Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff" )
-  #from Configuration.AlCa.GlobalTag import GlobalTag
-  #process.GlobalTag = GlobalTag( process.GlobalTag, '76X_mcRun2_asymptotic_v12' )
 
 else:
   OutputName = "_Data"
@@ -63,14 +69,20 @@ process.pf = cms.EDAnalyzer("OffsetTreeMaker",
     puFileName = cms.string("lumi-per-bx.root"),
     isMC = isMC,
     writeCands = cms.bool(True),
-    trackTag = cms.InputTag("generalTracks"),
-    pfTag = cms.InputTag("particleFlow"),
-    pvTag = cms.InputTag("offlinePrimaryVertices"),
-    muTag = cms.InputTag("addPileupInfo"),
+    #trackTag = cms.InputTag("generalTracks"),
+    #pfTag = cms.InputTag("particleFlow"),
+    #pvTag = cms.InputTag("offlinePrimaryVertices"),
+    #muTag = cms.InputTag("addPileupInfo"),
+    GenParticles = cms.InputTag("prunedGenParticles"),
+    genTag = cms.InputTag("packedGenParticles"),
+    pfTag = cms.InputTag("packedPFCandidates"),
+    pvTag = cms.InputTag("offlineSlimmedPrimaryVertices"),
+    muTag = cms.InputTag("slimmedAddPileupInfo"),
     rhoTag = cms.InputTag("fixedGridRhoFastjetAll"),
     rhoC0Tag = cms.InputTag("fixedGridRhoFastjetCentralNeutral"),
     rhoCCTag = cms.InputTag("fixedGridRhoFastjetCentralChargedPileUp"),
-    pfJetTag = cms.InputTag("ak4PFJetsCHS")
+    #pfJetTag = cms.InputTag("ak4PFJetsCHS")
+    pfJetTag = cms.InputTag("slimmedJets")
 )
 
 process.myseq = cms.Sequence( process.pf )
